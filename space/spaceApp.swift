ccaa6d6f7b5c5d3b4b26fb92022899191921d959
victor.agbeo <vicagbeo@outlook.com>
2023-10-16 18:07:14 -0700

    Initial Commit

---
+++ space/spaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceApp.swift
+//  space
+//
+//  Created by vicagbeo on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct spaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
